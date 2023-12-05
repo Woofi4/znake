6d1c4499e4c6706ddcc12c3110a7019e096f7b11
Islam Umakhanov <umakhanofficial@icloud.com>
2023-12-05 23:49:38 +0300

    Gameplay example

--- src/game.cpp
+++ src/game.cpp
@@ -38,14 +38,40 @@ bool game::load() {
 }
 
 void game::drawGame(sf::RenderWindow& window) {
+	std::string direction = "EAST";
+	snake snake(drawable_block(100, 100, game::system::blockSize), system::direction.at(direction));
+
+	unsigned tick = 0;
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) { window.close(); }
+			else if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::W && direction != "SOUTH") {
+					direction = "NORTH";
+				}
+				else if (event.key.code == sf::Keyboard::A && direction != "EAST") {
+					direction = "WEST";
+				}
+				else if (event.key.code == sf::Keyboard::S && direction != "NORTH") {
+					direction = "SOUTH";
+				}
+				else if (event.key.code == sf::Keyboard::D && direction != "WEST") {
+					direction = "EAST";
+				}
+
+				snake.setDirection(game::system::direction.at(direction));
+			}
 		}
 
 		window.clear();
+		for (const drawable_block& block : snake.getSnake()) { window.draw(block.getShape());}
 		window.display();
+
+		if (tick % (game::system::maxSpeed - game::system::speed) == 0) { snake.move(); }
+		
+		++tick;
+		if (tick == game::window::framerate) { tick = 0; }
 	}
 };
 
